6dca0f45731c4ded22c3ae162b4272fce6664bd1
stuparluka <101460322+stuparluka@users.noreply.github.com>
2022-05-29 23:34:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function javiSe(){
+	alert("Pozdrav");
+}
+function SacuvajIme(){
+	let ime = prompt("Kako se zoves?");
+	localStorage.setItem('ime', ime);
+	
+}
+ /*document.querySelector('#ey').addEventListener('click', function(){
+	document.getElementById("Lest").innerHTML - "Event works";});
+	
+let el = document.querySelector('#ime');
+if(!localStorage.getItem('ime')) {
+	sacuvajIme();	
+}
+el.textContent = localStorage.getItem('ime');*/
+
+	
+	 
